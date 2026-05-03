9e271ae41f084f1f1137d009e359759e9a9dc303
Usman9889 <usman9889a@gmail.com>
2026-05-03 10:58:09 +0530

    Count how many of the given numbers are: Positive Negative Even Odd

---
+++ loops/countNumbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    long long x;
+ 
+    int positive = 0, negative = 0, even = 0, odd = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        if (x > 0) positive++;
+        if (x < 0) negative++;
+        if (x % 2 == 0) even++;
+        else odd++;
+    }
+    cout << positive << endl;
+    cout << negative << endl;
+    cout << even << endl;
+    cout << odd << endl;
+ 
+}

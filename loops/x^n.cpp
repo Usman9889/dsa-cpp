dc5a65d90168f4c1dce9a20b795b36c5dbf65941
Usman9889 <usman9889a@gmail.com>
2026-05-07 20:56:06 +0530

    Print the value of x^n

---
+++ loops/x^n.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    long long factorial = 1;
+    for (int i = n; i > 0; i--) {
+        factorial = factorial *i;
+    }
+    cout << factorial;
+ 
+}

864411306a08966b3c4119cf250e9ff80be1bf5e
Usman9889 <usman9889a@gmail.com>
2026-05-19 23:06:57 +0530

    Print the Number in Reverse

--- loops/reverseNumber.cpp
+++ loops/reverseNumber.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
- 
 int main() {
     string N;
     cin >> N;
@@ -9,7 +8,6 @@ int main() {
     for (int i = N.length() - 1; i >= 0; i--) {
         cout << N[i];
     }
- 
     cout << endl;
 ;
 }

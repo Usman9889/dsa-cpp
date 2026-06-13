57ba716a18e70d14607d6d2e3459ad8dfae58d6c
Usman9889 <usman9889a@gmail.com>
2026-06-13 19:53:39 +0530

    Maximum Element with its position

---
+++ Arrays/MinimumElement.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i <= n - 1; i++) {
+        cin >> a[i];
+
+    }
+    long long ans = a[0], location = 1;
+    for (int i = 0; i <= n - 1; i++) {
+        if (a[i] < ans) {
+            ans = a[i];
+            location = i + 1;
+        }
+    }
+    cout << ans << " " << location << endl;
+
+}

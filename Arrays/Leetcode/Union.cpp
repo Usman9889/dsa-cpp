8794e7d589f89cbeffe424fde228c25ab0490c83
Usman9889 <usman9889a@gmail.com>
2026-06-21 22:28:55 +0530

    Union of two sorted Arrays, Optimal

---
+++ Arrays/Leetcode/Union.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    int n, m;
+    cin >> n >> m;
+    int a[n], b[m];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int j = 0; j < m; j++) {
+        cin >> b[j];
+    }
+    int i = 0;
+    int j = 0;
+    vector < int > unionArr;
+
+    while (i < n && j < m) {
+        if (a[i] <= b[j]) {
+            if (unionArr.empty() || unionArr.back() != a[i]) {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else {
+            if (unionArr.empty() || unionArr.back() != b[j]) {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+    while (i < n) {
+        if (unionArr.empty() || unionArr.back() != a[i]) {
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
+    while (j < m) {
+        if (unionArr.empty() || unionArr.back() != b[j]) {
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+    for (int i = 0; i < unionArr.size(); i++) {
+        cout << unionArr[i] << " ";
+    }
+}

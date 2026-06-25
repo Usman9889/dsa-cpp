be80a1af8a3384c2e60f7a092eb31745d7167ebf
Usman9889 <usman9889a@gmail.com>
2026-06-25 14:38:08 +0530

    Max Consecutive Ones, Optimal

---
+++ Arrays/Leetcode/485.maxConsecutiveOnes.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int maximum = 0;
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 1) {
+            count++;
+            maximum = max(count, maximum);
+        } else {
+            count = 0;
+        }
+    }
+    cout << maximum;
+}

a11f2197e82e371d4aba914e79bbbd17da1f23f2
Usman9889 <usman9889a@gmail.com>
2026-06-27 01:50:59 +0530

    2 Sum

--- Arrays/Leetcode/2sum.cpp
+++ Arrays/Leetcode/2sum.cpp
@@ -29,3 +29,30 @@ int main() {
 
     return 0;
 }
+
+// 2. Better (hashing)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int target;
+    cin >> target;
+
+    map<int, int> mpp;
+
+    for(int i=0; i<n; i++){
+      int num = a[i];
+      int moreElement = target - num;
+      if(mpp.find(moreElement) != mpp.end()){
+            cout << mpp[moreElement] << "," << i;
+      }
+      mpp[num] = i;
+    }
+    
+}

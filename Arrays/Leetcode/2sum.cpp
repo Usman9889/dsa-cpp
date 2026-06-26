8809d760c372ab7256214c8ab73bbd4a9eef261f
Usman9889 <usman9889a@gmail.com>
2026-06-27 01:43:24 +0530

    2 Sum

---
+++ Arrays/Leetcode/2sum.cpp
@@ -0,0 +1,31 @@
+// Approaches:
+// 1. Brute Force:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int a[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    int target;
+    cin >> target;
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+
+            if(a[i] + a[j] == target){
+                cout << i << " " << j << endl;
+                return 0;        // Exit after finding the answer
+            }
+        }
+    }
+
+    return 0;
+}

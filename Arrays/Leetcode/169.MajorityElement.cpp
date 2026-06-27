a25deeace7d217b451470709d2dc760b9a61eeec
Usman9889 <usman9889a@gmail.com>
2026-06-27 21:00:26 +0530

    Majority Element that occurs more than N/2 times

--- Arrays/Leetcode/169.MajorityElement.cpp
+++ Arrays/Leetcode/169.MajorityElement.cpp
@@ -50,3 +50,35 @@ int main() {
       }
     }
 }
+// 3. Optimal(Moore's voting algorithm)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    int cnt = 0;
+    int el;
+    for(int i=0; i<n; i++){
+      if(cnt == 0){
+            cnt = 1;
+            el = a[i];
+      }else if(a[i] == el){
+            cnt++;
+      }else{
+            cnt--;
+      }
+    }
+    int cnt1 = 0;
+    for(int i=0; i<n; i++){
+      if(a[i] == el) cnt1++;
+    }
+    if(cnt1 > n/2){
+      cout << el;
+    }
+}

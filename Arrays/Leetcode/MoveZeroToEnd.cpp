177ed7e93260cbbc3ffc760f326ae7690ae8bc74
Usman9889 <usman9889a@gmail.com>
2026-06-28 15:47:08 +0530

    Move Zero To End

--- Arrays/Leetcode/MoveZeroToEnd.cpp
+++ Arrays/Leetcode/MoveZeroToEnd.cpp
@@ -1,5 +1,7 @@
+// Brute Force Approach: The extremely naive solution, we can think of, involves the use of an extra array. 
+// Time Complexity: O(N), we can move all zeroes to end in linear time.
+// Space Complexity: O(N), additional space used for temporary array.
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
@@ -28,6 +30,38 @@ int main() {
     }
 
 
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+}
+// Optimal Approach: We can optimize the approach using 2 pointers i.e. i and j. The pointer j will point to the first 0 in the array and i will point to the next index.
+// Time Complexity: O(N), we can move all zeroes to end in linear time.
+// Space Complexity: O(1), constant additional space is used.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int j = -1;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+
+    for (int i = j + 1; i < n; i++) {
+        if (a[i] != 0) {
+            swap(a[i], a[j]);
+            j++;
+        }
+    }
     for (int i = 0; i < n; i++) {
         cout << a[i] << " ";
     }

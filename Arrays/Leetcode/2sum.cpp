e037705a872b8608b9bd51fdc2c211cf2fe44668
Usman9889 <usman9889a@gmail.com>
2026-06-30 02:05:20 +0530

    2 sum

--- Arrays/Leetcode/2sum.cpp
+++ Arrays/Leetcode/2sum.cpp
@@ -56,3 +56,39 @@ int main() {
     }
     
 }
+
+// 3.Optimal Approach(sorted array)
+// If array is not sorted: we will first sort the array and then try to choose the numbers in a greedy way.
+// We will keep a left pointer at the first index and a right pointer at the last index. Now until left < right, we will check the sum of arr[left] and arr[right]. Now if the sum < target, we need bigger numbers and so we will increment the left pointer. But if sum > target, we need to consider lesser numbers and so we will decrement the right pointer.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int, int> twoSum(vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (sum == target)
+            return {left, right};
+
+        else if (sum < target)
+            left++;
+
+        else
+            right--;
+    }
+
+    return {-1, -1};
+}
+
+int main() {
+    vector<int> arr = {2, 4, 7, 11, 15};
+    int target = 18;
+
+    auto ans = twoSum(arr, target);
+
+    cout << ans.first << " " << ans.second;
+}

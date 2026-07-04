776b3e58fdefe9f7fd485d68681606670976739e
Usman9889 <usman9889a@gmail.com>
2026-07-04 21:58:59 +0530

    Leaders in an Array

--- Arrays/Leetcode/Leader.cpp
+++ Arrays/Leetcode/Leader.cpp
@@ -22,5 +22,33 @@ int main(){
                   ans.push_back(a[i]);
             }
       }
-    
+      for(int i=0; i<ans.size(); i++){
+            cout << ans[i] << " ";
+      }
+}
+
+// Optimal
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+      int n;
+      cin >> n;
+      int a[n];
+      for(int i=0; i<n; i++){
+            cin >> a[i];
+      }
+      vector<int> ans;
+      int maxFromRight = INT_MIN;
+      
+      for(int i=n-1; i>=0; i--){
+            if(a[i] > maxFromRight){
+                  ans.push_back(a[i]);
+            }
+            maxFromRight = max(maxFromRight, a[i]);
+      }
+      reverse(ans.begin(), ans.end());
+      for(int i=0; i<ans.size(); i++){
+            cout << ans[i] << " ";
+      }
 }

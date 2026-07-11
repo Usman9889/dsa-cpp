b9f50d3b99699a6ba52d0cd5fe308e8725bb1877
Usman9889 <usman9889a@gmail.com>
2026-07-11 18:47:21 +0530

    Set Matrix Zeroes

--- Arrays/Leetcode/73.setMatrixZeroes.cpp
+++ Arrays/Leetcode/73.setMatrixZeroes.cpp
@@ -1,4 +1,67 @@
-//Brute Force Approach:
+#include <bits/stdc++.h>
+using namespace std;
+
+// Mark all elements in the given row as -1 (except existing zeros)
+void markRow(vector<vector<int>> &arr, int row, int m) {
+    for (int j = 0; j < m; j++) {
+        if (arr[row][j] != 0) {
+            arr[row][j] = -1;
+        }
+    }
+}
+
+// Mark all elements in the given column as -1 (except existing zeros)
+void markCol(vector<vector<int>> &arr, int col, int n) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i][col] != 0) {
+            arr[i][col] = -1;
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> arr(n, vector<int>(m));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    // Traverse each cell
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] == 0) {
+                markRow(arr, i, m);
+                markCol(arr, j, n);
+            }
+        }
+    }
+
+    // Replace all -1 with 0
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] == -1) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    // Print the modified matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+//Better Approach: Using extra row and first column as markers
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -6,44 +69,33 @@ int main(){
       int n, m;
       cin >> n >> m;
       int arr[n][m];
+
       for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                   cin >> arr[i][j];
             }
       }
-       // Mark all elements in this row as -1 (except existing zeros)
-      void markRow(int row){
-            for(int j=0; j<m; j++){
-                  if(arr[row][j] != 0){
-                        arr[row][j] = -1;
-                  }
-            }
-      }
-       // Mark all elements in this column as -1 (except existing zeros)
-      void markCol(int col){
-            for(int i=0; i<n; i++){
-                  if(arr[i][col] != 0){
-                        arr[i][col] = -1;
-                  }
-            }
-      }
-
-      // Traverse each cell of the matrix
+      int col[m] = {0};
+      int row[n] = {0};
       for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                  // If current cell is zero
                   if(arr[i][j] == 0){
-                           markRow(i);
-                           markCol(j);
+                        row[i] = 1;
+                        col[j] = 1;
                   }
             }
       }
-      //replace all -1 markers with 0
       for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                  if(arr[i][j] == -1){
+                  if(row[i] == 1 || col[j] == 1){
                         arr[i][j] = 0;
                   }
             }
+      }
+      for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                  cout << arr[i][j] << " ";
+            }
+            cout << endl;
       }
 }

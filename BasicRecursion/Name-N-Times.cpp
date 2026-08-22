95ed63fe63ea0c7cdb2eeff1c3396ef48a1d2d2b
Usman9889 <usman9889a@gmail.com>
2026-08-22 14:20:19 +0530

    name n times, sum of natural numbers

--- BasicRecursion/Name-N-Times.cpp
+++ BasicRecursion/Name-N-Times.cpp
@@ -12,3 +12,21 @@ int main(){
       cin >> n;
       printname(1, n);
 }
+//Sum of natural numbers using recursion
+#include <iostream>
+using namespace std;
+
+int sum(int  n){
+    
+    // base condition
+    if (n == 1)
+     return  1 ;
+     
+    return n + sum(n - 1); 
+}
+
+int main() {
+    int n = 5 ;
+    cout <<  sum(n); 
+    return 0;
+}

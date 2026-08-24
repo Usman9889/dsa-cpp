5648b2f5f95281e6077c59176e291486560e55f1
Usman9889 <usman9889a@gmail.com>
2026-08-24 13:46:52 +0530

    Second Largest Element

--- Arrays/Leetcode/SecondLargest.cpp
+++ Arrays/Leetcode/SecondLargest.cpp
@@ -21,6 +21,8 @@ int main(){
 }
 
 //Better(Two pass search)
+//Time complexity: O(2n) = 0(n)
+//Space complexity: O(1)
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -41,6 +43,32 @@ int main(){
       //Finding the second largest element
       for(int i=0; i<n; i++){
             if(arr[i] > secondLargest && arr[i] < largest){
+                  secondLargest = arr[i];
+            }
+      }
+      cout << secondLargest << endl;
+}
+
+//Optimal Approach
+//Time complexity: O(n)
+//Space complexity: O(1)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+      int n;
+      cin >> n;
+      vector<int> arr(n);
+      for(int i=0; i<n; i++){
+            cin >> arr[i];
+      }
+      int largest = -1, secondLargest = -1;
+      for(int i=0; i<n; i++){
+            if(arr[i] > largest){
+                  secondLargest = largest;
+                  largest = arr[i];
+            }
+            else if(arr[i] > secondLargest && arr[i] < largest){
                   secondLargest = arr[i];
             }
       }

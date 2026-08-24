b8a37cd97c3af082506b590f8c30ebf0079559f2
Usman9889 <usman9889a@gmail.com>
2026-08-24 14:06:34 +0530

    Check if an array is sorted and roated

---
+++ Arrays/Leetcode/1752.sortedAndRoated.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+      int n;
+      cin >> n;
+      vector<int> arr(n);
+      for(int i=0; i<n; i++){
+            cin >> arr[i];
+      }
+      int cnt = 0;
+      for(int i=0; i<n; i++){
+            if(arr[i] > arr[(i+1)%n]){
+                  cnt++;
+            }
+      }      
+      cout << (cnt <= 1 ? "YES" : "NO") << endl;
+}
+//Time complexity: O(n)
+//Space complexity: O(1)

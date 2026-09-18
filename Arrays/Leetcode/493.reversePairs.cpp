a1eb0926a1d956653858ce55a98f72451162b460
Usman9889 <usman9889a@gmail.com>
2026-09-18 20:27:35 +0530

    reverse pairs

---
+++ Arrays/Leetcode/493.reversePairs.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+void merge(vector<int> &arr, int low, int mid, int high){
+      int left = low;
+      int right = mid + 1;
+      vector<int> temp;
+      while(left <= mid && right <= high){
+            if(arr[left] <= arr[right]){
+                  temp.push_back(arr[left]);
+                  left++;
+            }else{
+                  temp.push_back(arr[right]);
+                  right++;
+            }
+      }
+      while(left <= mid){
+            temp.push_back(arr[left]);
+            left++;
+      }
+      while(right <= high){
+            temp.push_back(arr[right]);
+            right++;
+      }
+}
+int countPairs(vector<int> &arr, int low, int mid, int high){
+      int cnt = 0;
+      int right = mid + 1;
+      for(int i = low; i <= mid; i++){
+            while(right <= high && arr[i] > 2*arr[right]) right++;
+            cnt = cnt + (right - (mid + 1));
+      }
+      return cnt;
+}
+int mergeSort(vector<int> &arr, int low, int high){
+      int cnt =0;
+      if(low >= high) return cnt;
+      int mid = (low + high) / 2;
+      cnt += mergeSort(arr, low, mid);
+      cnt += mergeSort(arr, mid + 1, high);
+      cnt += countPairs(arr, low, mid, high);
+      merge(arr, low, mid, high);
+      return cnt;
+}
+int main(){
+      vector<int> arr = {12, 11, 13, 5, 6, 7};
+      int ans = mergeSort(arr, 0, arr.size() - 1);
+
+      cout << "Reverse pairs: " << ans << endl;
+
+      return 0;
+}
